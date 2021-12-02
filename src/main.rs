5d061438b4de65a09c22036cbfb5b04e73f48dcb
Sam Stelle <sam@creativex.com>
2021-12-02 11:24:34 -0800

    added vs code and day2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,82 @@
-mod depth;
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("No such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn num_increases(depths: Vec<i64>) -> i64 {
+    let mut previous_depth = f64::INFINITY as i64;
+    let mut increase_count = 0;
+    for current_depth in depths {
+        if current_depth > previous_depth {
+            increase_count += 1;
+        }
+        previous_depth = current_depth;
+    }
+    return increase_count;
+}
+
+fn sum_three(depths: Vec<i64>) -> Vec<i64> {
+
+    let mut three_sums: Vec<i64> = Vec::new();
+
+    for i in 2..depths.len() {
+        three_sums.push(depths[i-2] + depths[i-1] + depths[i]);
+    }
+    return three_sums;
+}
+
+fn count_depth() {
+    let lines = lines_from_file("data/day1");
+
+    let depths = lines.iter().map(|l| l.parse::<i64>().expect("Cannot parse int")).collect();
+    
+    let three_sum_depths = sum_three(depths);
+
+    let increase_count = num_increases(three_sum_depths);    
+
+    println!("Number of increases: {}", increase_count);
+}
+
+fn movement() {
+    let moves = lines_from_file("data/day2");
+
+    let mut depth = 0;
+    let mut aim = 0;
+    let mut horizontal_position = 0;
+
+    for m in moves {
+        let mut move_command = m.split_whitespace();
+        let direction = move_command.next().expect("Bad directions");
+        let distance = move_command.next().expect("How much").parse::<i64>().expect("Amount number");
+        match direction {
+            "forward" => {
+                horizontal_position += distance;
+                depth += aim * distance;
+            },
+            "down" => aim += distance,
+            "up" => aim -= distance,
+            _ => println!("wtf")
+        }
+    }
+    
+    println!("Depth: {}", depth);
+    println!("Distanct: {}", horizontal_position);
+    println!("Total: {}", depth * horizontal_position);
+
+}
 
 
 fn main() {
-    depth::count_depth();
+    count_depth();
+    movement();
 }
